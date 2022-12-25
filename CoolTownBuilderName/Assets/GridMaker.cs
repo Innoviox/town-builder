9fb8828b511bdc9e5adb5a668e35e3e486b9716d
Laura <llvb418@gmail.com>
2022-12-24 22:15:36 -0500

    grid

---
+++ CoolTownBuilderName/Assets/GridMaker.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridMaker : MonoBehaviour
+{
+    public int width, length;
+    public Transform gridSquare;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int y=0; y<length; y++)
+       {
+           for (int x=0; x<width; x++)
+           {
+               Instantiate(gridSquare, new Vector3(y,0,x), Quaternion.identity);
+           }
+       }    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
