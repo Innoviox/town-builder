385f8e973c507201d83faa5c806d688a538299b8
Laura <llvb418@gmail.com>
2022-12-27 23:17:18 -0500

    food and wood init

--- CoolTownBuilderName/Assets/Scripts/GridMaker.cs
+++ CoolTownBuilderName/Assets/Scripts/GridMaker.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GridMaker : MonoBehaviour
 {
@@ -9,11 +10,26 @@ public class GridMaker : MonoBehaviour
     private List<Transform> children;
     private Transform currentBuilding;
 
+    private Dictionary<ResourceItems.gameResources, int> res; 
+
+    public TMP_Text foodText, woodText;
+
     // Start is called before the first frame update
     void Start()
     {
         children = new List<Transform>();
         
+        res = new Dictionary<ResourceItems.gameResources, int>();
+        foreach(ResourceItems.gameResources type in System.Enum.GetValues(typeof(ResourceItems.gameResources)))
+        {
+            res.Add(type, 50);
+            
+        }
+
+        foodText.text = res[ResourceItems.gameResources.Food].ToString();
+        woodText.text = res[ResourceItems.gameResources.Wood].ToString();
+        
+
         for (int y = 0; y < length; y++)
         {
             for (int x = 0; x < width; x++)
@@ -23,16 +39,19 @@ public class GridMaker : MonoBehaviour
                 children.Add(t);
             }
         }    
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        foodText.text = res[ResourceItems.gameResources.Food].ToString();
+        woodText.text = res[ResourceItems.gameResources.Wood].ToString();
     }
 
     public void SetBuilding(Transform building) {
         currentBuilding = building;
+        
     }
 
     public Transform GetCurrentBuilding() {
