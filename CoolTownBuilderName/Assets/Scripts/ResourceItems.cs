3d5a2f54d20f878ea947d461fa12572b3f4e1926
Laura <llvb418@gmail.com>
2022-12-28 22:25:21 -0500

    Revert "Revert "food and wood init""
    This reverts commit 07a982ed28f6544527740e5cf9402ce62413d9b6.

---
+++ CoolTownBuilderName/Assets/Scripts/ResourceItems.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ResourceItems
+{
+    public enum gameResources 
+    {
+        Wood,
+        Food
+    }
+    public gameResources gameRes;
+}
