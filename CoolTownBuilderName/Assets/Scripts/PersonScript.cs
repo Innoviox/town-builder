90dbcd84d68d329d42751ed0e27a216467562183
Simon Chervenak <sarras305@gmail.com>
2023-01-04 22:49:19 -0500

    starting jobs

--- CoolTownBuilderName/Assets/Scripts/PersonScript.cs
+++ CoolTownBuilderName/Assets/Scripts/PersonScript.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class PersonScript : MonoBehaviour
 {
+    public Job job = Job.None;
+    private Dictionary<Job, float> skills;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        skills = new Dictionary<Job, float>();
+        foreach(Job j in System.Enum.GetValues(typeof(Job)))
+        {
+            skills.Add(j, 1.0f);   
+        }
     }
 
     // Update is called once per frame
