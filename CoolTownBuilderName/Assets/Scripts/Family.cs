37892ef9af0ce22f900a44b2139c0b1d9aeb4b83
Laura <llvb418@gmail.com>
2023-01-04 23:00:48 -0500

    family

--- CoolTownBuilderName/Assets/Scripts/Family.cs
+++ CoolTownBuilderName/Assets/Scripts/Family.cs
@@ -3,5 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Family {
-    
+
+    public List<PersonScript> members;
+    public string name;
+    public static readonly List<string> nameOptions;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
 }
